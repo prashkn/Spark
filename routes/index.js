9a739f69cd647dc4d57d4d7acfd4e8ba11c874a5
neil784 <nprabh8@illinois.edu>
2022-11-13 19:11:57 -0600

    Initial Commit

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,5 @@
  * Connect all of your endpoints together here.
  */
 module.exports = function (app, router) {
-    app.use('/api', require('./home.js')(router));
+    app.use('/', require('./home.js')(router));
 };
