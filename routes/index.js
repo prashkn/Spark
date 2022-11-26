02f0b47db7883f5f149811e0cb9cfcd6537da7cb
Prashant Naganaboyina <pnaganab@gmail.com>
2022-11-26 07:26:28 -0500

    Added /api prefix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
  * Connect all of your endpoints together here.
  */
 module.exports = function (app, router) {
-    app.use('/', require('./home.js')(router))
-    app.use('/users', require('./users.js'))
-    app.use('/projects', require('./projects'))
+  app.use("/api", require("./home.js")(router));
+  app.use("/api/users", require("./users.js"));
+  app.use("/api/projects", require("./projects"));
 };
