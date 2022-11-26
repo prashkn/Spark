914ad7211378beaae938024f0ffeb648a0acd871
Prashant Naganaboyina <pnaganab@gmail.com>
2022-11-26 11:06:59 -0600

    redirected db to finalprojectDB

--- config/secrets.js
+++ config/secrets.js
@@ -2,6 +2,7 @@
  * You generally want to .gitignore this file to prevent important credentials from being stored on your public repo.
  */
 module.exports = {
-    token : "secret-starter-mern",
-    mongo_connection : "mongodb+srv://Anyone:1234@finalproject.lv0nsvg.mongodb.net/?retryWrites=true&w=majority"
+  token: "secret-starter-mern",
+  mongo_connection:
+    "mongodb+srv://Anyone:1234@finalproject.lv0nsvg.mongodb.net/finalprojectDB?retryWrites=true&w=majority",
 };
