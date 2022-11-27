25a808f81636579974f93fc127e1059966e130b8
Owen Hayes <owen.c.hayes@gmail.com>
2022-11-26 21:39:26 -0600

    Remove inline styling

--- client/screens/Settings/Settings.js
+++ client/screens/Settings/Settings.js
@@ -1,55 +1,74 @@
-import { Button, Pressable, ScrollView, Text, View } from 'react-native';
+import { DefaultTheme } from '@react-navigation/native';
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { GAINSBORO, MIDNIGHT_GREEN } from '../../styles/palette';
-
-// TODO: Remove inline styling
 
 export default function Settings({ navigation }) {
   return (
     <ScrollView>
+      {/* Map each settings menu to a pressable button and separator */}
       {[
-        { name: 'Account Information', icon: 'person-circle-outline'},
+        { name: 'Account Information', icon: 'person-circle-outline' },
         { name: 'Security', icon: 'lock-closed-outline' },
         { name: 'Help', icon: 'help-outline' },
         { name: 'About', icon: 'information-circle-outline' },
       ].map((setting, i) => (
-        <Pressable onPress={() => navigation.push(setting.name)} key={i}>
-          <View
-            style={{
-              minHeight: 40,
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              paddingHorizontal: 30,
-              paddingVertical: 15,
-            }}
+        <>
+          <Pressable
+            // Navigate to menu when pressed
+            onPress={() => navigation.push(setting.name)}
+            key={i}
+            style={({ pressed, hovered }) => [
+              {
+                ...styles.menuButton,
+
+                // Make background color slightly darker when pressed/hovered
+                backgroundColor:
+                  pressed || hovered
+                    ? '#e8e8e8'
+                    : DefaultTheme.colors.background,
+              },
+            ]}
           >
             <Icon
               name={setting.icon}
               size={30}
               color={MIDNIGHT_GREEN}
-              style={{ marginRight: 5 }}
+              style={styles.menuIcon}
             />
-            <Text
-              style={{
-                fontFamily: 'Poppins-SemiBold',
-                fontSize: 20,
-                color: MIDNIGHT_GREEN,
-              }}
-            >
-              {setting.name}
-            </Text>
-          </View>
-          <View
-            style={{
-              borderBottomColor: GAINSBORO,
-              borderBottomWidth: 2,
-              borderRadius: 10,
-              marginHorizontal: 20,
-            }}
-          />
-        </Pressable>
+            <Text style={styles.menuText}>{setting.name}</Text>
+          </Pressable>
+
+          {/* Separator */}
+          <View style={styles.separator} />
+        </>
       ))}
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  menuButton: {
+    minHeight: 40,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+  },
+
+  menuIcon: { marginRight: 5 },
+
+  menuText: {
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 20,
+    color: MIDNIGHT_GREEN,
+  },
+
+  separator: {
+    borderBottomColor: GAINSBORO,
+    borderBottomWidth: 2,
+    borderRadius: 10,
+    marginHorizontal: 20,
+  },
+});
