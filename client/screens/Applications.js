1fa6e4dcc0a89cbe91b659c6bb956104241dde73
Prashant Naganaboyina <pnaganab@gmail.com>
2022-12-12 17:48:52 -0600

    ?

--- client/screens/Applications.js
+++ client/screens/Applications.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState, useContext } from 'react';
 import {
-    Pressable,
-    SafeAreaView,
-    ScrollView,
-    StyleSheet,
-    Text,
-    View,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
 } from 'react-native';
 import Project from '../components/Project';
 import { BLOND, MIDNIGHT_GREEN } from '../styles/palette';
@@ -13,114 +13,117 @@ import { Skeleton } from '@rneui/themed';
 import { BASE_URL } from '../data/util';
 import { UserContext } from '../components/UserContext';
 
-
 export const Applications = ({ navigation }) => {
-    const [applications, setApplications] = useState([]);
-    const NUM_OF_SKELETONS = 7;
-    const {user} = useContext(UserContext)
+  const [applications, setApplications] = useState([]);
+  const NUM_OF_SKELETONS = 7;
+  const { user } = useContext(UserContext);
 
-    const getApplications = async (id) => {
-        //! get applications
-        try {
-            const info = await fetch(
-                `${BASE_URL}/users/applications?userId=${id}`
-            );
-            const result = await info.json();
-            if (result.data.length === 0) {
-                setApplications([]);
-            } else {
-                for (let i = 0; i < result.data.length; i++) {
-                    setApplications(apps => [...apps, result.data[i]]);
-                }
-            }
-        } catch (err) {
-            console.log(err);
+  const getApplications = async (id) => {
+    //! get applications
+    try {
+      const info = await fetch(`${BASE_URL}/users/applications?userId=${id}`);
+      const result = await info.json();
+      if (result.data.length === 0) {
+        setApplications([]);
+      } else {
+        for (let i = 0; i < result.data.length; i++) {
+          setApplications((apps) => [...apps, result.data[i]]);
         }
-    };
-
-
-    const skeletons = [];
-    for (let i = 0; i < NUM_OF_SKELETONS; i++) {
-        skeletons.push(
-            <View style={styles.applications} key={i}>
-                <View style={styles.card}>
-                    <Skeleton animation="wave" width={345} height={90} />
-                </View>
-            </View>
-        );
+      }
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    useEffect(() => {
-        getApplications(user._id);
-    }, []);
-    
-    navigation.addListener('focus', () => {
-        // reset marker state
-        getApplications(user._id);
-      });
+  const skeletons = [];
+  for (let i = 0; i < NUM_OF_SKELETONS; i++) {
+    skeletons.push(
+      <View style={styles.applications} key={i}>
+        <View style={styles.card}>
+          <Skeleton animation="wave" width={345} height={90} />
+        </View>
+      </View>
+    );
+  }
 
-    return (
-        <SafeAreaView
-            style={{
-                backgroundColor: BLOND,
-                flex: 1,
-            }}
-        >
-            <ScrollView style={styles.container}>
-                <Text style={styles.header}>My Applications</Text>
-                {applications.length === 0 ?
-                    <View style={{ height: '100%', justifyContent: 'center' }}>
-                        <Text style={{ alignSelf: 'center', fontFamily: 'Poppins-Regular' }}>You have not applied to any projects.</Text>
-                    </View>
-                    :
-                    <View style={styles.applications}>
-                        {!applications ? (
-                            skeletons
-                        ) : (
-                            applications.map((app, i) => (
-                                <Pressable
-                                    onPress={() =>
-                                        navigation.navigate('Details', { id: app.projectId, fromApps: true })
-                                    }
-                                    key={i}
-                                >
-                                    <Project projectId={app.projectId} status={applications[i].status} />
-                                </Pressable>
-                            ))
-                        )}
-                    </View>
-                }
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
+  useEffect(() => {
+    getApplications(user._id);
+  }, []);
 
+  navigation.addListener('focus', () => {
+    // reset marker state
+    getApplications(user._id);
+  });
 
-const styles = StyleSheet.create({
-    container: {
+  return (
+    <SafeAreaView
+      style={{
         backgroundColor: BLOND,
         flex: 1,
-        paddingHorizontal: 18,
-    },
-    header: {
-        fontSize: 30,
-        fontFamily: 'Poppins-SemiBold',
-        color: MIDNIGHT_GREEN,
-    },
-    applications: {
-        alignItems: 'center',
-        display: 'flex',
-    },
-    card: {
-        backgroundColor: 'white',
-        borderRadius: 10,
-        margin: 5,
-        shadowOffset: { width: 3, height: 4 },
-        shadowColor: '#333',
-        shadowOpacity: 0.2,
-        shadowRadius: 5,
-        display: 'flex',
-        flexDirection: 'row',
-        marginBottom: 10,
-    },
-})
+      }}
+    >
+      <ScrollView style={styles.container}>
+        <Text style={styles.header}>My Applications</Text>
+        {applications.length === 0 ? (
+          <View style={{ height: '100%', justifyContent: 'center' }}>
+            <Text
+              style={{ alignSelf: 'center', fontFamily: 'Poppins-Regular' }}
+            >
+              You have not applied to any projects.
+            </Text>
+          </View>
+        ) : (
+          <View style={styles.applications}>
+            {!applications
+              ? skeletons
+              : applications.map((app, i) => (
+                  <Pressable
+                    onPress={() =>
+                      navigation.navigate('Details', {
+                        id: app.projectId,
+                        fromApps: true,
+                      })
+                    }
+                    key={i}
+                  >
+                    <Project
+                      projectId={app.projectId}
+                      status={applications[i].status}
+                    />
+                  </Pressable>
+                ))}
+          </View>
+        )}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: BLOND,
+    flex: 1,
+    paddingHorizontal: 18,
+  },
+  header: {
+    fontSize: 30,
+    fontFamily: 'Poppins-SemiBold',
+    color: MIDNIGHT_GREEN,
+  },
+  applications: {
+    alignItems: 'center',
+    display: 'flex',
+  },
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    margin: 5,
+    shadowOffset: { width: 3, height: 4 },
+    shadowColor: '#333',
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    display: 'flex',
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+});
