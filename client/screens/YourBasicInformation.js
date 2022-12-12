cda82ae8ab04ed8bd9248b5b0cae576f7ea8cf63
Owen Hayes <owen.c.hayes@gmail.com>
2022-12-12 05:16:47 -0600

    Fix ScrollView narrow display issue

--- client/screens/YourBasicInformation.js
+++ client/screens/YourBasicInformation.js
@@ -1,14 +1,12 @@
+import * as EmailValidator from 'email-validator';
 import { useContext, useState } from 'react';
-import { SafeAreaView, ScrollView, StatusBar, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar, Text, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import LoginButton from '../components/LoginButton';
 import LoginTextInput from '../components/LoginTextInput';
-import ProgressBar from '../components/ProgressBar';
-import Icon from 'react-native-vector-icons/Ionicons';
-import { GAINSBORO, MUSTARD } from '../styles/palette';
-import * as EmailValidator from 'email-validator';
-import WarningMessage from '../components/WarningMessage';
 import { NewUserInfoContext } from '../components/NewUserInfoContext';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import ProgressBar from '../components/ProgressBar';
+import WarningMessage from '../components/WarningMessage';
 
 export default function YourBasicInformation({ navigation }) {
   const { newUserInfo, setNewUserInfo } = useContext(NewUserInfoContext);
@@ -79,107 +77,112 @@ export default function YourBasicInformation({ navigation }) {
         }}
       >
         <ProgressBar progress={progress} />
-        <KeyboardAwareScrollView
+      </View>
+      <KeyboardAwareScrollView
+        style={{
+          height: '100%',
+        }}
+        contentContainerStyle={{
+          maxWidth: 600,
+          marginHorizontal: 'auto',
+          paddingHorizontal: 20,
+        }}
+      >
+        <Text style={{ fontFamily: 'Poppins-Bold', fontSize: 24 }}>
+          Your Basic Information
+        </Text>
+
+        <Text
           style={{
-            height: '100%',
+            fontFamily: 'Poppins-Regular',
+            fontSize: 14,
+            color: '#989898',
           }}
         >
-          <Text style={{ fontFamily: 'Poppins-Bold', fontSize: 24 }}>
-            Your Basic Information
-          </Text>
+          Welcome to Spark! We can't wait to see what amazing ideas you turn
+          into reality.
+        </Text>
 
-          <Text
-            style={{
-              fontFamily: 'Poppins-Regular',
-              fontSize: 14,
-              color: '#989898',
+        <View
+          style={{
+            marginTop: 20,
+          }}
+        >
+          {/* TODO: Add asterisks */}
+          <LoginTextInput
+            placeholder="Name"
+            onChangeText={(text) => {
+              setNewUserInfo({ ...newUserInfo, name: text });
             }}
-          >
-            Welcome to Spark! We can't wait to see what amazing ideas you turn
-            into reality.
-          </Text>
+            onSubmitEditing={submitForm}
+            returnKeyType="done"
+          />
 
-          <View
-            style={{
-              marginTop: 20,
-            }}
-          >
-            {/* TODO: Add asterisks */}
+          {/* TODO: Use object instead of individual booleans to store if fields are invalid and store a message in the object so warning message can be dynamically updated */}
+          {nameValid === false && (
+            <WarningMessage message="Please enter your full name." />
+          )}
+
+          <View>
             <LoginTextInput
-              placeholder="Name"
+              placeholder="username"
               onChangeText={(text) => {
-                setNewUserInfo({ ...newUserInfo, name: text });
+                setNewUserInfo({ ...newUserInfo, username: text });
               }}
               onSubmitEditing={submitForm}
               returnKeyType="done"
+              style={{ paddingLeft: 28 }}
             />
-
-            {/* TODO: Use object instead of individual booleans to store if fields are invalid and store a message in the object so warning message can be dynamically updated */}
-            {nameValid === false && (
-              <WarningMessage message="Please enter your full name." />
-            )}
-
-            <View>
-              <LoginTextInput
-                placeholder="username"
-                onChangeText={(text) => {
-                  setNewUserInfo({ ...newUserInfo, username: text });
-                }}
-                onSubmitEditing={submitForm}
-                returnKeyType="done"
-                style={{ paddingLeft: 28 }}
-              />
-              <Text
-                style={{
-                  fontFamily: 'Poppins-Medium',
-                  fontSize: 16,
-                  color: '#BDBDBD',
-                  position: 'absolute',
-                  paddingLeft: 10,
-                  paddingTop: 12
-                  // top: '22%',
-                  // left: 10,
-                }}
-              >
-                @
-              </Text>
-            </View>
-
-            {usernameValid === false && (
-              <WarningMessage message="Please enter a username." />
-            )}
-
-            <LoginTextInput
-              placeholder="Email"
-              keyboardType="email-address"
-              onChangeText={(text) => {
-                setNewUserInfo({ ...newUserInfo, email: text });
+            <Text
+              style={{
+                fontFamily: 'Poppins-Medium',
+                fontSize: 16,
+                color: '#BDBDBD',
+                position: 'absolute',
+                paddingLeft: 10,
+                paddingTop: 12,
+                // top: '22%',
+                // left: 10,
               }}
-              onSubmitEditing={submitForm}
-              returnKeyType="done"
-            />
-
-            {emailValid === false && (
-              <WarningMessage message="Please enter a valid email address." />
-            )}
-
-            <LoginTextInput
-              placeholder="Password"
-              secureTextEntry
-              onChangeText={(text) => {
-                setNewUserInfo({ ...newUserInfo, password: text });
-              }}
-              onSubmitEditing={submitForm}
-              returnKeyType="done"
-            />
-
-            {passwordValid === false && (
-              <WarningMessage message="Please enter a password with at least 8 characters." />
-            )}
+            >
+              @
+            </Text>
           </View>
-          <LoginButton title="Continue" onPress={submitForm} />
-        </KeyboardAwareScrollView>
-      </View>
+
+          {usernameValid === false && (
+            <WarningMessage message="Please enter a username." />
+          )}
+
+          <LoginTextInput
+            placeholder="Email"
+            keyboardType="email-address"
+            onChangeText={(text) => {
+              setNewUserInfo({ ...newUserInfo, email: text });
+            }}
+            onSubmitEditing={submitForm}
+            returnKeyType="done"
+          />
+
+          {emailValid === false && (
+            <WarningMessage message="Please enter a valid email address." />
+          )}
+
+          <LoginTextInput
+            placeholder="Password"
+            secureTextEntry
+            onChangeText={(text) => {
+              setNewUserInfo({ ...newUserInfo, password: text });
+            }}
+            onSubmitEditing={submitForm}
+            returnKeyType="done"
+          />
+
+          {passwordValid === false && (
+            <WarningMessage message="Please enter a password with at least 8 characters." />
+          )}
+        </View>
+        <LoginButton title="Continue" onPress={submitForm} />
+      </KeyboardAwareScrollView>
 
       <StatusBar barStyle="dark-content" />
     </SafeAreaView>
